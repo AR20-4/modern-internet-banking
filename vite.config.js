57f70a726288c02aef62093f22d9fd147798730f
Rachel Glasgow <RachelNicoleGlasgow@gmail.com>
2024-02-06 22:49:13 -0600

    Update vite.config.js
    same test

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/modern_internet_banking/",
+  base: "/modern-internet-banking/",
   plugins: [react()],
 })
